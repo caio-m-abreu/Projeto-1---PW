237507072642f10a2c875d55b2010fa1b22897a6
caio-m-abreu <caioabreu2318@gmail.com>
2025-04-02 12:29:56 -0300

    estilos-cadastro-login

--- views/cadastrar.php
+++ views/cadastrar.php
@@ -1,1 +1,19 @@
-<p>CADASTRO</p>
+<section class="cadastro">
+    <div class="cadastro__container">
+        <h2 class="cadastro__titulo">Cadastrar</h2>
+        <form action="#" method="POST" class="cadastro__form">
+            
+            <label for="nome" class="cadastro__label">Nome</label>
+            <input type="text" class="cadastro__input" id="nome" name="nome" placeholder="Nome" required>
+            
+            <label for="email" class="cadastro__label">E-mail</label>
+            <input type="email" class="cadastro__input" id="email" name="email" placeholder="E-mail" required>
+            
+            <label for="senha" class="cadastro__label">Senha</label>
+            <input type="password" class="cadastro__input" id="senha" name="senha" placeholder="Senha" required>
+            
+            <button type="submit" class="cadastro__button">Cadastrar-se</button>
+            <p class="cadastro__descricao">Já tem uma conta? Clique <a href="?bbb=login"><b>aqui</b></a> para entrar</p>
+        </form>
+    </div>
+</section>
--- views/login.php
+++ views/login.php
@@ -1,1 +1,15 @@
-<p>LOGIN</p>
+<section class="cadastro">
+    <div class="cadastro__container">
+        <h2 class="cadastro__titulo">Entrar</h2>
+        <form action="#" method="POST" class="cadastro__form">
+            <label for="email" class="cadastro__label">E-mail</label>
+            <input type="email" class="cadastro__input" id="email" name="email" placeholder="E-mail" required>
+            
+            <label for="senha" class="cadastro__label">Senha</label>
+            <input type="password" class="cadastro__input" id="senha" name="senha" placeholder="Senha" required>
+            
+            <button type="submit" class="cadastro__button">Entrar</button>
+            <p class="cadastro__descricao">Ainda não tem conta?? Clique <a href="?bbb=cadastrar"><b>aqui</b></a> para cadastrar-se</p>
+        </form>
+    </div>
+</section>
